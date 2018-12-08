fa4db633da47827d2f5556fed3c81fbc0cd9affd
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2018-12-08 17:54:52 +0100

    presentation version of the serverless part

--- flinkster_sls/actions/src/index.js
+++ flinkster_sls/actions/src/index.js
@@ -1,18 +1,17 @@
 const rp = require('request-promise-native');
-const authorization = 'Bearer b4be0828de244da82e6574cf902e38c5' // by account hd4db
-
+const authorization = 'Bearer b4be0828de244da82e6574cf902e38c5'; // by account hd4db
+const ors_apikey = '5b3ce3597851110001cf6248e578d992470742c7a90a961cebca66c3';
 /**
  *
  * @param { lat, lon, radius}
  *
  * radius: in meter (max. 10.000)
  */
-async function main({ lat = '52.524945', lon = '13.369661', radius = 10000 } = {}) {
+async function main({ lat = '52.523430', lon = '13.411440', radius = 1000, calcWalkDistance = false } = {}) {
     const proposals = await bookingproposals({ lat, lon, radius });
-    let items = updateDistance({lat, lon}, proposals.items);
-    // TODO: calculate item.distance
-    // TODO: sort by distance
-    return {items};
+    let items = proposals.items;
+    await updateDistanceForAll(items, lat, lon, calcWalkDistance);
+    return { items };
 }
 
 async function bookingproposals({ lat, lon, radius } = {}) {
@@ -30,7 +29,6 @@ async function bookingproposals({ lat, lon, radius } = {}) {
         },
         json: true // Automatically parses the JSON string in the response
     };
-
     return rp(options);
 }
 
@@ -38,14 +36,63 @@ async function bookingproposals({ lat, lon, radius } = {}) {
  * @param {} position
  * @param [] items
  */
-function updateDistance({ lat, lon}, items) {
+async function updateDistanceForAll(items, lat, lon, calcWalkDistance) {
     const geodist = require('geodist');
-    items.forEach(item => {
-        item.distance = geodist({lat, lon}, {lat: item.position.coordinates[0], lon: item.position.coordinates[1]})
-    });
-    items.sort((item1,item2) => item1.distance - item2.distance)
-    return items;
+    for (const item of items) {
+        itemLat = item.position.coordinates[1];
+        itemLon = item.position.coordinates[0];
+        // calc the distance
+        if (calcWalkDistance) {
+            // note: this request the api of openrouteservice.org
+            try {
+                const routeSummary = await directions({
+                    lat,
+                    lon,
+                    itemLat,
+                    itemLon
+                });
+                item.distance = routeSummary.distance;
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            let distance = geodist({ lat, lon }, { lat: itemLat, lon: itemLon }, { exact: true, unit: 'meters' })
+            item.distance = Math.round(distance);
+        }
+        // add the mapUrl
+        item.mapUrl = `https://maps.openrouteservice.org/directions?n1=${lat}&n2=${lon}&n3=14&a=${lat},${lon},${itemLat},${itemLon}&b=2&c=0&g1=-1&g2=0&k1=de-DE&k2=km`
+    };
+    return Promise.resolve(items);
+}
+
+/**
+ * Calculates the direction based on the given positions using openrouteservice.org
+ * @see https://openrouteservice.org/dev/#/api-docs/directions/get
+ * @param {*} param
+ */
+async function directions({ lat, lon, itemLat, itemLon } = {}) {
+    const options = {
+        uri: 'https://api.openrouteservice.org/directions',
+        qs: {
+            api_key: ors_apikey,
+            coordinates: `${lon},${lat}|${itemLon},${itemLat}`,
+            profile: 'foot-walking'
+        },
+        json: true // Automatically parses the JSON string in the response
+    };
+    try {
+        console.log('openrouteservice options: ' + JSON.stringify(options));
+        const result = await rp(options);
+        // console.log('openrouteservice result: ' + result);
+        return Promise.resolve({
+            distance: Math.round(result.routes[0].summary.distance),
+            duration: result.routes[0].summary.duration
+        })
+    } catch (error) {
+        return Promise.reject(error.message);
+    }
 }
 
 //exports.demo = demoAction;
 exports.main = main;
+exports.directions = directions;
--- flinkster_sls/actions/test/index.test.js
+++ flinkster_sls/actions/test/index.test.js
@@ -4,8 +4,8 @@ const index = require('../src/index');
 describe('asserts', () => {
 
   it('check booking proposals', async () => {
-    const response = await index.main()
-    console.log(response);
+    const response = await index.main({radius: 10000})
+    //console.log(response);
     const distances = [];
     response.items.forEach(item => {
       distances.push(item.distance);
@@ -14,5 +14,27 @@ describe('asserts', () => {
     console.log(distances);
   });
 
+  it('check direction calc', async () => {
+    const directionParams = {
+      lat: 52.524945,
+      lon: 13.369661,
+      itemLat: 52.524398,
+      itemLon: 13.366157
+    }
+    const response = await index.directions(directionParams)
+    console.log(response);
+    assert.ok(response.distance >= 0, `Expected distance to be >= 0 | was "${response.distance}"`);
+    assert.ok(response.duration >= 0, `Expected duration to be >= 0 | was "${response.duration}"`);
+  });
 
+  it('get booking proposals with walkDistance', async () => {
+    const response = await index.main({calcWalkDistance: true})
+    //console.log(response);
+    const distances = [];
+    response.items.forEach(item => {
+      distances.push(item.distance);
+      assert.ok(item.distance >= 0, `Expected distance to be >= 0 | was "${item.distance}"`);
+    });
+    console.log(distances);
+  });
 });
